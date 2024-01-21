b2713bc400a510b862999262d12f535f404177c4
Levani Birchadze <levanbirchadze@gmail.com>
2024-01-21 12:58:17 +0400

    add color switcher

---
+++ main.js
@@ -0,0 +1,16 @@
+const root_colors = document.querySelector(":root");
+const theme_switch = document.querySelector(".switch-theme");
+
+let isDarkMode = false;
+
+theme_switch.addEventListener("click", () => {
+  isDarkMode = !isDarkMode;
+
+  if (isDarkMode) {
+    root_colors.style.setProperty("--txt-color", "#fff");
+    root_colors.style.setProperty("--background-color", "#000");
+  } else {
+    root_colors.style.setProperty("--txt-color", "#000");
+    root_colors.style.setProperty("--background-color", "#fff");
+  }
+});
